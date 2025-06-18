66fd431ecbd6e371868c93e2442cad70a490840f
Jae Chan Kwon <flcat.xd@gmail.com>
2025-06-18 12:01:15 +0900

    컨벤션 추가

---
+++ src/test/java/com/jamjaris/PhanesEditor/PhanesEditorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamjaris.PhanesEditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhanesEditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
