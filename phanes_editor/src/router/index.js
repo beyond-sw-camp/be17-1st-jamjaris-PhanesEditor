61fa636f8e86327d32732d72045739ce2ed5c7f9
Jae Chan Kwon <flcat.xd@gmail.com>
2025-06-24 17:33:14 +0900

    Landing 페이지 초안

--- phanes_editor/src/router/index.js
+++ phanes_editor/src/router/index.js
@@ -1,15 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import EditorView from '../views/EditorView.vue'
 import CreateProjectPage from '@/components/CreateProjectPage.vue'
 import SignUpPage from '@/views/SignUpPage.vue'
+import LandingPage from '@/views/LandingPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+        name: 'landing',
+        component: LandingPage
+    },
+    {
+    path: '/editor',
+      name: 'editor',
+      component: EditorView
     },
     {
       path: '/create',
